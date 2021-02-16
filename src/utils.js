9a9bcbf90fd2d8c85ca1bbf8150c00f9bbfd1a1f
iggyfo <iggyfo@gmail.com>
2021-02-16 10:57:02 +0300

    Исправляет ошибки прошлого задания

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 import {Offer} from "./const";
 
 
-export const random = (a = 1, b = 0) => {
+export const getRandom = (a = 1, b = 0) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const getOfferNum = () => random(Offer.MIN_OFFERS, Offer.MAX_OFFERS);
+export const getOfferNum = () => getRandom(Offer.MIN_OFFERS, Offer.MAX_OFFERS);
