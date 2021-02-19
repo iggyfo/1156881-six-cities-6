31c8bc221c980958b4146818a5dc4c1a4039a206
iggyfo <iggyfo@gmail.com>
2021-02-20 00:36:19 +0300

    Переписывает favorite-card на функциональный компонент

--- src/components/favorite-card/favorite-card.jsx
+++ src/components/favorite-card/favorite-card.jsx
@@ -1,54 +1,48 @@
-import React, {Component} from "react";
+import React from "react";
 import {offerPropsTypes} from "../../props-types";
 import propTypes from "prop-types";
 
 
-class FavoriteCard extends Component {
-  constructor(props) {
-    super(props);
-  }
+const FavoriteCard = ({offer}) => {
 
-  render() {
-    const {previewImage, title, type, price} = this.props.offer;
-    return (
-      <article className="favorites__card place-card">
-        <div className="favorites__image-wrapper place-card__image-wrapper">
-          <a href="#">
-            <img className="place-card__image" src={previewImage} width={150} height={110} alt="Place image" />
-          </a>
-        </div>
-        <div className="favorites__card-info place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">€{price}</b>
-              <span className="place-card__price-text">/&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-              <svg className="place-card__bookmark-icon" width={18} height={19}>
-                <use xlinkHref="#icon-bookmark" />
-              </svg>
-              <span className="visually-hidden">In bookmarks</span>
-            </button>
+  const {previewImage, title, type, price} = offer;
+  return (
+    <article className="favorites__card place-card">
+      <div className="favorites__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={previewImage} width={150} height={110} alt="Place image" />
+        </a>
+      </div>
+      <div className="favorites__card-info place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">€{price}</b>
+            <span className="place-card__price-text">/&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{width: `100%`}} />
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <a href="#">{title}</a>
-          </h2>
-          <p className="place-card__type">{type}</p>
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+            <svg className="place-card__bookmark-icon" width={18} height={19}>
+              <use xlinkHref="#icon-bookmark" />
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
         </div>
-      </article>
-    );
-  }
-}
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `100%`}} />
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{title}</a>
+        </h2>
+        <p className="place-card__type">{type}</p>
+      </div>
+    </article>
+  );
+};
 
 FavoriteCard.propTypes = {
-  offer: propTypes.shape(offerPropsTypes),
-  onMouseEnter: propTypes.func,
+  offer: propTypes.shape(offerPropsTypes).isRequired,
 };
 
 export default FavoriteCard;
