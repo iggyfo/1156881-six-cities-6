a9947a5fd610e74faa5949daf6c8fa5a9d91c8d4
iggyfo <iggyfo@gmail.com>
2021-02-26 20:29:28 +0300

    Добавляет компонент header.jsx

--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import propTypes from "prop-types";
 import Cities from "../cities/cities";
 import Map from "../map/map";
-import {nanoid} from "nanoid";
 import OfferList from "../offer-list/offer-list";
+import Header from "../header/header";
+import {nanoid} from "nanoid";
 
 
 const MainScreen = ({offers, offerNum, userAuth, cities}) => {
@@ -12,28 +13,7 @@ const MainScreen = ({offers, offerNum, userAuth, cities}) => {
 
   return (
     <React.Fragment>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">{userAuth}</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header userAuth={userAuth} />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
