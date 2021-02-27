f90bc9caf67fbe485a9ce676168b946edf0a43bb
iggyfo <iggyfo@gmail.com>
2021-02-27 14:20:37 +0300

    Удаляет закомментированный код

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -20,9 +20,6 @@ const Header = ({userAuth}) => {
             <Link className="header__logo-link" to="/">
               <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />
             </Link>
-            {/*<a className="header__logo-link" href="main.html">*/}
-            {/*  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width={81} height={41} />*/}
-            {/*</a>*/}
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
