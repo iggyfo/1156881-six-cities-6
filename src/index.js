4d860951a3f72d56344efb7c31f8357e648fb80e
iggyfo <iggyfo@gmail.com>
2021-04-05 14:09:51 +0300

    Добавляет создание стора

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
 import {createApi} from "./services/api";
 import {ActionCreator} from './store/action';
 import App from './components/app/app';
@@ -10,7 +8,7 @@ import {nearPlaces} from "./mock/nerby";
 import {reviews} from "./mock/reviews";
 import {Provider} from 'react-redux';
 import rootReducer from './store/root-reducer';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {configureStore} from '@reduxjs/toolkit';
 import {checkAuth} from "./store/api-actions";
 import {AuthorizationStatus} from "./const";
 import {redirect} from "./store/middlewares/redirect";
@@ -21,13 +19,15 @@ const api = createApi(
     () => store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH))
 );
 
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-        applyMiddleware(thunk.withExtraArgument(api)),
-        applyMiddleware(redirect)
-    )
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      },
+    }).concat(redirect)
+});
 
 store.dispatch(checkAuth());
 
